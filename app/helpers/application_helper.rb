4736b4000440f701246b790090ec6deb37f1d6ff
Brad Thompson <bkt@brad-thompson.com>
2024-05-29 20:06:17 -0400

    7 set up team types, teams, and memberships

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def contrasting_color(color)
+    base = Color::RGB.by_hex(color)
+    if base.brightness > 0.5
+      '#000'
+    else
+      '#fff'
+    end
+  end
 end
