76f6d7fb0f4df0aedab04c71fb354cfcce6abd4e
Brad Thompson <bkt@brad-thompson.com>
2024-06-07 19:27:53 -0400

    8 move pundit controller module to internal controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  include Pundit::Authorization
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
