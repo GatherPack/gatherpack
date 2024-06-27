168440e62a35360afd7a6ccbea249f05e6e3b8f4
Brad Thompson <bkt@brad-thompson.com>
2024-06-27 16:22:01 -0400

    more windows fixes

--- db/migrate/20240617175249_create_events.rb
+++ db/migrate/20240617175249_create_events.rb
@@ -1,13 +1,13 @@
 class CreateEvents < ActiveRecord::Migration[8.0]
   def change
-    create_table :events, id: :uuid do |t|
+    create_table :events, id: KeyTypePicker.key_type do |t|
       t.text :name
       t.text :description
       t.datetime :start_time
       t.datetime :end_time
       t.string :location
-      t.references :event_type, null: false, foreign_key: true, type: :uuid
-      t.references :team, null: true, type: :uuid #do not need to have a team for an event
+      t.references :event_type, null: false, foreign_key: true, type: KeyTypePicker.key_type
+      t.references :team, null: true, type: KeyTypePicker.key_type #do not need to have a team for an event
 
       t.timestamps
     end
