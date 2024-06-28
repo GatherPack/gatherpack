64b347eb8f21dc253e0ca411a35956a6de9e8e22
codemob <69110900+commandblox@users.noreply.github.com>
2024-06-28 16:24:44 -0400

    color picker!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,16 @@ import * as bootstrap from "bootstrap"
 FontAwesome.config.mutateApproach = 'sync'
 import "trix"
 import "@rails/actiontext"
+
+document.addEventListener("turbo:load", ev => {
+    document.querySelectorAll('.fancy-color-container>input[type="color"]').forEach(elem => {
+        elem.addEventListener("input", ev => {
+            ev.target.nextElementSibling.value = ev.target.value
+        });
+    });
+    document.querySelectorAll('.fancy-color-container>input[type="text"]').forEach(elem => {
+        elem.addEventListener("input", ev => {
+            ev.target.previousElementSibling.value = ev.target.value
+        });
+    });
+})
