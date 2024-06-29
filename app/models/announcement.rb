b004419d0c62cfe2a1a71c88c178a0cd4f8bf3dc
Ian Wobschall <ian.wobschall@gmail.com>
2024-06-28 20:56:19 -0400

    added scope

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,6 +1,8 @@
 class Announcement < ApplicationRecord
   belongs_to :team, optional: true
 
+  scope :visible, -> { where("start_time < ?", DateTime.now).and(where("end_time > ?", DateTime.now)) }
+
   def self.ransackable_attributes(auth_object = nil)
     ['title', 'content', 'start_time', 'end_time', 'team_id']
   end
