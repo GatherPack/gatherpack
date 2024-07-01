b6ef6d1aea621db5ff9d628a4120ff5528e9c5ef
Gerseneck <wang14240@gmail.com>
2024-07-01 16:40:14 -0400

    added relationship validations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,18 @@ class Relationship < ApplicationRecord
   belongs_to :relationship_type
   belongs_to :parent, class_name: 'Person'
   belongs_to :child, class_name: 'Person'
+
+  validate :different_parent_and_child
+
+  private
+    def different_parent_and_child
+      if parent.nil? or child.nil?
+        return
+      end
+
+      if parent.id == child.id
+        errors.add(:parent, 'and Child must be different')
+        errors.add(:child, 'and Parent must be different')
+      end
+    end
 end
