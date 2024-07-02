88f6a9cfd4df2ad4d992b36d4f688d19401d4276
codemob <69110900+commandblox@users.noreply.github.com>
2024-07-02 14:00:35 -0400

    icons, dismissing, and colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,27 @@ module ApplicationHelper
       team_as_badge(obj)
     end.html_safe
   end
+
+  def flash_to_class(type)
+    case type
+    when 'notice'
+      bootstrap_type = 'info';
+    else
+      bootstrap_type = type;
+    end
+
+    "alert-" + bootstrap_type
+  end
+  def flash_to_icon(type)
+    case type
+    when 'success'
+      bootstrap_icon = '#check-circle-fill'
+    when 'warning', 'danger'
+      bootstrap_icon = '#exclamation-triangle-fill'
+    else
+      bootstrap_icon = '#info-fill'
+    end
+
+    bootstrap_icon
+  end
 end
