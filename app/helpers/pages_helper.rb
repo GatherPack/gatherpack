982da780e1e12079887f972543cbc73ede57882a
Brad Thompson <bkt@brad-thompson.com>
2024-07-11 21:36:15 -0400

    37 PR feedback

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def format_page_content(page)
+    if page.dynamic
+      simple_format ERB.new(page.content).result(binding)
+    else
+      simple_format page.content
+    end
+  end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -3,11 +3,13 @@ class Page < ApplicationRecord
 
   PERMISSION_LEVELS = %w[ public user team manager admin ]
 
+  validates :title, presence: true
+  validates :content, presence: true
   validates :viewer, inclusion: { in: PERMISSION_LEVELS }
   validates :editor, inclusion: { in: PERMISSION_LEVELS }
-  validates :editor, inclusion: { in: ['admin'], message: 'must be restricted to admins when enabling ERB parsing'}, if: -> { dynamic == true }
-  validate :manager_editor_needs_team
-  validate :manager_viewer_needs_team
+  validates :editor, inclusion: { in: [ 'admin' ], message: 'must be restricted to admins when enabling ERB parsing' }, if: -> { dynamic == true }
+  validate :manager_permission_needs_team
+  validate :permission_hierarchy
 
   def self.ransackable_attributes(auth_object = nil)
     [ 'content', 'title', 'team_id' ]
@@ -19,11 +21,12 @@ class Page < ApplicationRecord
 
   private
 
-  def manager_editor_needs_team
+  def manager_permission_needs_team
     errors.add(:editor, "can't be 'manager' if there's no team") if editor == 'manager' && team.nil?
+    errors.add(:viewer, "can't be 'manager' if there's no team") if viewer == 'manager' && team.nil?
   end
 
-  def manager_viewer_needs_team
-    errors.add(:viewer, "can't be 'manager' if there's no team") if viewer == 'manager' && team.nil?
+  def permission_hierarchy
+    errors.add(:viewer, "can't be higher than the editor permission") if PERMISSION_LEVELS.find_index(editor) < PERMISSION_LEVELS.find_index(viewer)
   end
 end
