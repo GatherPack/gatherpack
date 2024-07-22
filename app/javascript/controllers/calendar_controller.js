a63be66b8f1a07d3042da76913ff28050dc69b6a
Gerseneck <wang14240@gmail.com>
2024-07-22 12:39:03 -0400

    changed timezone of calendar to UTC to match model

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -16,6 +16,7 @@ export default class extends Controller {
       fixedWeekCount: false,
       weekNumbers: true,
       dayMaxEventRows: true,
+      timeZone: "UTC",
 
       headerToolbar: {
         left: "prev,next today",
