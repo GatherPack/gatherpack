46aa718f1ebe25ab1384df9c252d483f13984a53
codemob <69110900+commandblox@users.noreply.github.com>
2024-07-22 19:56:23 -0400

    Fixed code nitpicks

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,7 +1,10 @@
 class SettingsController < ApplicationController
     before_action :check_for_admin
+
     def index
+
     end
+
     def update
         params[:settings].each do |key, val|
             Settings.set(key.to_sym, val)
