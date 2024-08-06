db9492d2b791b3e8b45e1f179aac7d222bf38ba4
ritachiu0703 <ritachiu.0703@gmail.com>
2024-08-06 16:49:12 -0400

    added identifier_mame method and turbo

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,17 @@
 class Token < ApplicationRecord
   belongs_to :tokenable, polymorphic: true
+
+
+  def identifier_name
+    "#{tokenable.identifier_name} - #{value.truncate(10)}"
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "tokenable_id", "tokenable_type", "updated_at", "value"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["tokenable"]
+  end
+
 end
