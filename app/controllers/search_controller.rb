e17b2845ef13bede80576964df2de9f139be8d19
Brad Thompson <bkt@brad-thompson.com>
2024-09-15 13:32:01 -0400

    10 post-merge updates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,8 @@ class SearchController < ApplicationController
     events = policy_scope(Event).ransack(name_or_description_cont: params[:q]).result(distinct: true)
     teams = policy_scope(Team).ransack(name_cont: params[:q]).result(distinct: true)
     badges = policy_scope(Badge).ransack(name_or_description_cont: params[:q]).result(distinct: true)
-    @results = people + events + teams + badges
+    tokens = policy_scope(Token).ransack(value_cont: params[:q]).result(distinct: true)
+    announcements = policy_scope(Announcement).ransack(name_or_content_cont: params[:q]).result(distinct: true)
+    @results = people + events + teams + badges + announcements + tokens
   end
 end
