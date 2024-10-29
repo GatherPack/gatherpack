7b0689a9421155f824de5e31c47aac235cb3ab34
ez500 <eugenezhang500@gmail.com>
2024-10-29 19:36:52 -0400

    finish requested changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,7 @@ badge_type = BadgeType.find_or_create_by!(name: "Test Badge Type")
 users = []
 persons = []
 
-8.times do |time|
-  users << User.find_or_create_by!(email: "test#{time}@gmail.com") do |user|
-    user.password = "12345678"
-  end
+10.times do |time|
   persons << Person.find_or_create_by!(first_name: "Test First Name #{time}",
                                      last_name: "Test Last Name #{time}",
                                      display_name: "Test Display Name #{time}",
@@ -26,8 +23,7 @@ persons = []
                                      phone_number: "Test Phone Number#{time}",
                                      address: "Test Address#{time}",
                                      birthday: "1991-11-11",
-                                     dietary_restrictions: "Test Dietary Restrictions#{time}",
-                                     user: users[time])
+                                     dietary_restrictions: "Test Dietary Restrictions#{time}")
 end
 
 team = Team.find_or_create_by!(name: "Test Team", color: "#f5c211", team_type: team_type, join_permission: "added_by_admin")
@@ -57,11 +53,11 @@ variable = Variable.find_or_create_by!(name: "test_variable",
                                        klass: "string",
                                        raw_value: "\"Test String Variable Content\"")
 report = Report.find_or_create_by!(name: "Test Report",
-                                   code: "def test_report_function\n \"Test Report Content\"\nend\ntest_report_function")
+                                   code: "\"Test Report Content\"")
 hook = Hook.find_or_create_by!(name: "Test Hook",
                                event: "announcement - update",
-                               code: "def test_hook_function\n \"Test Hook Content\"\nend\ntest_hook_function")
-4.times do |time|
+                               code: "\"Test Hook Content\"")
+5.times do |time|
   badge_assignment = BadgeAssignment.find_or_create_by!(person: persons[time * 2], badge: badge)
-  membership = Membership.find_or_create_by!(team: team, person: persons[time * 2 + 1], manager: false)
+  membership = Membership.find_or_create_by!(team: team, person: persons[time * 2], manager: false)
 end
