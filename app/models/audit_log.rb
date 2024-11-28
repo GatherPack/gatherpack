f7268733764083954391b90beaa0fa2baf9b1dd3
Gerseneck <wang14240@gmail.com>
2024-11-28 18:04:04 -0500

    feat: more search params for audit log

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -2,4 +2,8 @@ class AuditLog < AbstractVersion
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "event", "id", "item_id", "item_type", "object", "object_changes", "whodunnit"]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["item"]
+  end
 end
