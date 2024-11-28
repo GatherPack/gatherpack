42ee291292ec548e93396eedb3e2b87d49865c8b
Gerseneck <wang14240@gmail.com>
2024-11-28 17:50:57 -0500

    feat: audit log searching

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,2 +1,5 @@
 class AuditLog < AbstractVersion
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "event", "id", "item_id", "item_type", "object", "object_changes", "whodunnit"]
+  end
 end
