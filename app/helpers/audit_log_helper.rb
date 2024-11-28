593302d69ee7fae3a313078dc234fe06134e0f3c
Gerseneck <wang14240@gmail.com>
2024-11-28 18:13:23 -0500

    chore: cleanup

--- app/helpers/audit_log_helper.rb
+++
@@ -1,2 +0,0 @@
-module AuditLogHelper
-end
--- test/controllers/audit_logs_controller_test.rb
+++ test/controllers/audit_logs_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class AuditLogControllerTest < ActionDispatch::IntegrationTest
+class AuditLogsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
