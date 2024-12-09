12ce3043ef9f3bc1c7f5099094bdf7a46a098480
Gerseneck <wang14240@gmail.com>
2024-12-09 17:52:11 -0500

    feat: separate db for versions (should be working)

--- app/models/abstract_version.rb
+++ app/models/abstract_version.rb
@@ -1,4 +1,6 @@
 class AbstractVersion < PaperTrail::Version
   include PaperTrail::VersionConcern
   self.abstract_class = true
+
+  connects_to database: { writing: :versions, reading: :versions }
 end
