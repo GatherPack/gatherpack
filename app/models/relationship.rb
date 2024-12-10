eae562b38518e3e66856cc4c0ba6db354cf3a853
Brad Thompson <bkt@brad-thompson.com>
2024-12-10 16:58:02 -0500

    41 fixing up relationship stuff

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,29 @@ class Relationship < ApplicationRecord
   belongs_to :relationship_type
   belongs_to :parent, class_name: :Person
   belongs_to :child, class_name: :Person
+
+  validate :no_self_relationships
+
+  attr_accessor :start_node, :node_occupant
+
+  def reify
+    raise ArgumentError unless start_node.present? && node_occupant.present?
+    
+    type_id, side = start_node.split(':')
+    self.relationship_type = RelationshipType.find(type_id)
+    
+    if side == 'p'
+      self.parent = node_occupant
+    elsif side == 'c'
+      self.child = node_occupant
+    else
+      raise ArgumentError
+    end
+  end
+
+  private
+
+  def no_self_relationships
+    errors.add(:parent, "cannot be the same person as the child") if parent != nil && parent == child
+  end
 end
--- app/models/relationship_type.rb
+++ app/models/relationship_type.rb
@@ -1,6 +1,6 @@
 class RelationshipType < ApplicationRecord
   has_many :relationships
-  PERMISSION_LEVELS = %w[ parent child user team manager admin ]
+  enum :permission, added_by_admin: 0, added_by_manager: 1, added_by_team_member: 2, added_by_participant: 3, added_by_user: 4
 
   def self.ransackable_attributes(auth_object = nil)
     [ 'parent_label', 'child_label' ]
