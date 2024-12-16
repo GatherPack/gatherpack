d53beb093f7225dc140ca5e40b1c5e9a2558cb1a
ez500 <eugenezhang500@gmail.com>
2024-12-16 12:14:01 -0500

    search time clock period, temporary show all time clock periods

--- app/models/time_clock_period.rb
+++ app/models/time_clock_period.rb
@@ -1,4 +1,12 @@
 class TimeClockPeriod < ApplicationRecord
   belongs_to :team, optional: true
   enum :permission, added_by_admin: 0, added_by_manager: 1, added_by_team_member: 2, added_by_participant: 3, added_by_user: 4
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[ team_id start_time end_time ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[ team ]
+  end
 end
--- app/models/time_clock_punch.rb
+++ app/models/time_clock_punch.rb
@@ -1,4 +1,12 @@
 class TimeClockPunch < ApplicationRecord
   belongs_to :person
   belongs_to :time_clock_period, optional: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[ person_display_name start_time end_time time_clock_period_id ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[ person time_clock_period ]
+  end
 end
