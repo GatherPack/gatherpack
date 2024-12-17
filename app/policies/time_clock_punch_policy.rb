a79c82d1026822a814958fa7eb4ffa07563fc58c
ez500 <eugenezhang500@gmail.com>
2024-12-17 14:40:35 -0500

    refine punch permissions

--- app/policies/time_clock_punch_policy.rb
+++ app/policies/time_clock_punch_policy.rb
@@ -10,4 +10,16 @@ class TimeClockPunchPolicy < ApplicationPolicy
       end
     end
   end
+
+  def update?(record)
+    person.managed_people.include?(record.person) || person == record.person && record.time_clock_period.nil? ? true : %w[ added_by_user added_by_team_member ].include?(record.time_clock_period.permission)
+  end
+
+  def edit?(record)
+    update?(record)
+  end
+
+  def destroy?(record)
+    update?(record)
+  end
 end
