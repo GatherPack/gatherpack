775630c912230c2f365b2ca77a4a497c468e13b0
ez500 <eugenezhang500@gmail.com>
2024-12-18 00:37:47 -0500

    date formatting and refined permission check & keys

--- db/migrate/20241214191735_create_time_clock_periods.rb
+++ db/migrate/20241214191735_create_time_clock_periods.rb
@@ -2,8 +2,8 @@ class CreateTimeClockPeriods < ActiveRecord::Migration[8.0]
   def change
     create_table :time_clock_periods, id: :uuid do |t|
       t.string :name
-      t.datetime :start_time
-      t.datetime :end_time
+      t.date :start_time
+      t.date :end_time
       t.integer :permission, default: 0, null: false
       t.references :team, null: true, foreign_key: true, type: :uuid
 
