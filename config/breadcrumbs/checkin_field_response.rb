473359547cdd63cdab1589ce1ea4b57973eae206
Gerseneck <wang14240@gmail.com>
2024-12-18 21:42:18 -0500

    fix: why did i generate this as a scaffold and not just a model

--- config/breadcrumbs/checkin_field_response.rb
+++
@@ -1,8 +0,0 @@
-crumb :checkin_field_responses do
-  link "Checkin_field_responses", checkin_field_responses_path
-end
-
-crumb :checkin_field_response do |checkin_field_response|
-  link checkin_field_response.identifier_name, checkin_field_response
-  parent :checkin_field_responses
-end
