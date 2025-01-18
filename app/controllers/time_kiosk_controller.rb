6d919c2865f6a93a7121194e3aaaf8e748e951ed
Gerseneck <wang14240@gmail.com>
2025-01-18 11:13:33 -0500

    fix: use `find_by_rfid` to find token

--- app/controllers/time_kiosk_controller.rb
+++ app/controllers/time_kiosk_controller.rb
@@ -3,22 +3,14 @@ class TimeKioskController < ApplicationController
   end
 
   def create
-    @token = policy_scope(Token).find_by_value(normalize_id(time_kiosk_params[:token_id]))
+    @token = policy_scope(Token).find_by_rfid(time_kiosk_params[:token_id])
 
-    puts @token
+    puts @token.value
   end
 
   private
 
     def time_kiosk_params
       params.require(:time_kiosk).permit(:token_id)
-    end
-
-    def normalize_id(rfid)
-      if rfid.to_s.match?(/\A5700[0-9a-fA-F]+\z/)
-        rfid = rfid.to_s[4..-1].to_i(16).to_s
-      end
-
-      rfid
     end
 end
