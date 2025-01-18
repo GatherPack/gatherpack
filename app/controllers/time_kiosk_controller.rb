855e3c10ca48755ea310e5d19dac02efc1910438
Gerseneck <wang14240@gmail.com>
2025-01-18 15:36:37 -0500

    fix: use different layout for kiosk

--- app/controllers/time_kiosk_controller.rb
+++ app/controllers/time_kiosk_controller.rb
@@ -2,10 +2,11 @@ class TimeKioskController < ApplicationController
   def index
     @accept_input = true
     @message = {}
+    render layout: "kiosk"
   end
 
   def create
-    @token = policy_scope(Token).find_by_rfid(time_kiosk_params[:token_id])
+    @token = Token.find_by_rfid(time_kiosk_params[:token_id])
     @accept_input = true
     @message = {}
 
@@ -15,15 +16,15 @@ class TimeKioskController < ApplicationController
 
     case @token&.tokenable_type
     when "Hook"
-      @hook = policy_scope(Hook).find_by_id(@token.tokenable_id)
+      @hook = Hook.find_by_id(@token.tokenable_id)
       @message[:info] = @hook.run(@hook.code)
     when "Person"
-      @person = policy_scope(Person).find_by_id(@token.tokenable_id)
+      @person = Person.find_by_id(@token.tokenable_id)
 
-      current_punches = policy_scope(TimeClockPunch).all.where(person: @person, end_time: nil)
+      current_punches = TimeClockPunch.all.where(person: @person, end_time: nil)
       current_punches.each do |punch|
         punch.update!(end_time: Time.now)
-        @message[:info] = "Clocked out #{@person.identifier_name}"
+        @message[:info] = "Punched out #{@person.identifier_name}"
       end
 
       if current_punches.empty?
@@ -31,7 +32,7 @@ class TimeKioskController < ApplicationController
       end
     end
 
-    render :index
+    render :index, layout: "kiosk"
   end
 
   private
