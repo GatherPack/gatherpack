3e9ab418898c516cb7969d451762c6d54955de1d
Gerseneck <wang14240@gmail.com>
2025-01-18 13:30:44 -0500

    feat: better alert messages

--- app/controllers/time_kiosk_controller.rb
+++ app/controllers/time_kiosk_controller.rb
@@ -1,23 +1,29 @@
 class TimeKioskController < ApplicationController
   def index
     @accept_input = true
+    @message = {}
   end
 
   def create
     @token = policy_scope(Token).find_by_rfid(time_kiosk_params[:token_id])
     @accept_input = true
+    @message = {}
+
+    unless @token
+      @message[:danger] = "Invalid Token"
+    end
 
     case @token&.tokenable_type
     when "Hook"
       @hook = policy_scope(Hook).find_by_id(@token.tokenable_id)
-      @message = @hook.run(@hook.code)
+      @message[:info] = @hook.run(@hook.code)
     when "Person"
       @person = policy_scope(Person).find_by_id(@token.tokenable_id)
 
       current_punches = policy_scope(TimeClockPunch).all.where(person: @person, end_time: nil)
       current_punches.each do |punch|
         punch.update!(end_time: Time.now)
-        @message = "Clocked out #{@person.identifier_name}"
+        @message[:info] = "Clocked out #{@person.identifier_name}"
       end
 
       if current_punches.empty?
