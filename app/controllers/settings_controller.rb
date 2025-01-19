3e1d2c99aa1e451429b3348603b044d427c5db0e
Gerseneck <wang14240@gmail.com>
2025-01-19 11:15:23 -0500

    feat: one save button for settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,13 +1,14 @@
 class SettingsController < ApplicationController
-    before_action :check_for_admin
+  before_action :check_for_admin
 
-    def index
+  def index
 
+  end
+
+  def update
+    params[:settings].each do |key, val|
+      Settings.set(key.to_sym, val)
     end
-
-    def update
-        params[:settings].each do |key, val|
-            Settings.set(key.to_sym, val)
-        end
-    end
+    redirect_to settings_url, notice: "Settings were successfully updated.", status: :see_other
+  end
 end
