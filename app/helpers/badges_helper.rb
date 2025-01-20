df84dd6430aeb12c7e44418a1d9d1f8a640fa5ff
Gerseneck <wang14240@gmail.com>
2025-01-20 14:57:35 -0500

    fix: badge icon not loading correctly after search + hover over description

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,7 +1,7 @@
 module BadgesHelper
   def badge_as_badge(badge)
     link_to badge, class: 'undecorated' do
-      tag.span badge.short, class: 'badge', style: "background-color: #{badge.color}; color: #{contrasting_color(badge.color)}"
+      tag.span i(badge.short), class: 'badge', style: "background-color: #{badge.color}; color: #{contrasting_color(badge.color)}", title: badge.identifier_name
     end
   end
 end
