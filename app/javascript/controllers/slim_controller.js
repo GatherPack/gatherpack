42355591f891ea4876c11295c695ab31c8bbd107
Gerseneck <wang14240@gmail.com>
2025-02-20 18:21:55 -0500

    feat: slightly better styling for slim select

--- app/javascript/controllers/slim_controller.js
+++ app/javascript/controllers/slim_controller.js
@@ -5,7 +5,11 @@ import SlimSelect from "slim-select"
 export default class extends Controller {
   connect() {
     new SlimSelect({
-      select: this.element
+      select: this.element,
+      settings: {
+        placeholderText: "",
+        allowDeselect: true,
+      }
     })
   }
 }
