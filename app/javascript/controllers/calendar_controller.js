c3d565afc3a3341d2d61b303de8d76654edcecf8
Gerseneck <wang14240@gmail.com>
2025-07-24 18:41:48 -0400

    feat: better calendar view settings

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -54,7 +54,7 @@ export default class extends Controller {
             "Content-Type": "application/json",
             "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content
           },
-          body: JSON.stringify(Object.assign({ "start_time": info.start.toISOString(), "end_time": info.end.toISOString(), "q": this.parse_params(document.getElementsByTagName("turbo-frame")[0])}, this.get_show_settings()))
+          body: JSON.stringify(Object.assign({ "start_time": info.start.toISOString(), "end_time": info.end.toISOString(), "q": this.parse_params(document.getElementsByTagName("turbo-frame")[0])}, this.get_view_settings()))
         }).then((response) => response.json())
           .then((data) => {
             successfulCallback(data)
@@ -64,21 +64,33 @@ export default class extends Controller {
 
     calendar.render()
 
-    document.querySelectorAll(".form-check-input").forEach((checkbox) => {
-      if (!(checkbox.id.match(/[a-z]+-check/))) { return }
-      
-      checkbox.addEventListener("change", () => {
-        calendar.refetchEvents()
-      })
-    })
+    if (document.querySelectorAll("[id^='calendar-']").length != 0) { 
+      this.render_initial_settings(calendar)
+    }
   }
 
-  get_show_settings() {
+  get_view_settings() {
     return {
-      birthdays: document.getElementById("birthdays-check")?.checked ?? true,
-      events: document.getElementById("events-check")?.checked ?? true,
-      notes: document.getElementById("notes-check")?.checked ?? true
+      birthdays: localStorage.getItem("calendar-birthdays") === "true" ?? true,
+      events: localStorage.getItem("calendar-events") === "true" ?? true,
+      notes: localStorage.getItem("calendar-notes") === "true" ?? true
     }
+  }
+
+  render_initial_settings(calendar) {
+    const initial_settings = this.get_view_settings()
+
+    document.getElementById("calendar-birthdays").checked = initial_settings.birthdays
+    document.getElementById("calendar-events").checked = initial_settings.events
+    document.getElementById("calendar-notes").checked = initial_settings.notes
+
+    document.getElementById("calendar-settings-submit").addEventListener("click", () => {
+      localStorage.setItem("calendar-birthdays", document.getElementById("calendar-birthdays").checked)
+      localStorage.setItem("calendar-events", document.getElementById("calendar-events").checked)
+      localStorage.setItem("calendar-notes", document.getElementById("calendar-notes").checked)
+
+      calendar.refetchEvents()
+    })
   }
 
   parse_params(el) {
