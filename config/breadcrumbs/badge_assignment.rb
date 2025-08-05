8c4b20464247507a6d8274284ca72d6e52ad267c
Gerseneck <wang14240@gmail.com>
2025-08-05 12:41:42 -0400

    fix: hide super long badges + badge assignment breadcrumbs

--- config/breadcrumbs/badge_assignment.rb
+++ config/breadcrumbs/badge_assignment.rb
@@ -4,6 +4,10 @@ crumb :badge_assignments do |badge|
 end
 
 crumb :badge_assignment do |badge_assignment|
-  link badge_assignment.new_record? ? "New badge assignment" : [ badge_assignment.identifier_name, badge_badge_assignment_path(badge_assignment.badge, badge_assignment) ]
+  if badge_assignment.new_record?
+    link "New badge assignment"
+  else
+    link badge_assignment.identifier_name, badge_badge_assignment_path(badge_assignment.badge, badge_assignment)
+  end
   parent :badge_assignments, badge_assignment.badge
 end
