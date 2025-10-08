1f9c11e981dfaf22702cbdae085af6bbb010dcc1
Brad Thompson <bkt@brad-thompson.com>
2025-10-08 17:09:41 -0400

    378 clean up some relationship-related policy gaps

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -3,8 +3,12 @@ module RelationshipsHelper
     other = host == relationship.parent ? relationship.child : relationship.parent
     label = host == relationship.parent ? relationship.relationship_type.child_label : relationship.relationship_type.parent_label
     content = i('user') + ' ' + "#{other.display_name} [#{label}]"
-    link_to other do
-      tag.span content, class: 'badge text-bg-dark'
+    if policy(other).show?
+      link_to other do
+        tag.span content, class: 'badge text-bg-dark'
+      end
+    else
+      tag.span content, class: 'badge text-bg-dark', title: "You don't have permission to view this person."
     end
   end
 end
