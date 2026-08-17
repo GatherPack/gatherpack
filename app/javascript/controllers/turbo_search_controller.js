1890639489790eb18a9cd447fc9d095fe12bbd69
Brad Thompson <bkt@brad-thompson.com>
2026-08-17 19:28:49 -0400

    maybe fix a search bug?

--- app/javascript/controllers/turbo_search_controller.js
+++ app/javascript/controllers/turbo_search_controller.js
@@ -3,7 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="turbo-search"
 export default class extends Controller {
   search(){
-    this.element.pushState({}, "")
+    const params = new URLSearchParams(new FormData(this.element))
+    const url = `${this.element.getAttribute("action")}?${params}`
+    history.pushState({}, "", url)
   }
 
   clear(){
